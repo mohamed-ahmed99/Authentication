adf61dc9637b7f70f4b1d5ea09345e1d053bc7e9
Mohamed <masmhdmas@gmail.com>
2025-09-01 17:01:22 +0300

    fix to deploy on vercel

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -56,5 +56,8 @@ const ConnectDB = async () => {
 ConnectDB()
 
 
-
-app.listen(PORT, () => console.log(`server is running`))
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+// app.listen(PORT, () => console.log(`server is running`))
+export default app
